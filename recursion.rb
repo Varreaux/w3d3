ae9b7a7867b9400b9e56374caea41587de138599
Amir Lamrabat <secretsofthecode9@gmail.com>
2023-01-25 13:50:08 -0500

    exponent versions

--- recursion.rb
+++ recursion.rb
@@ -1,18 +1,18 @@
-# def Warmup(min, max)
+def Warmup(min, max)
     
-#     # arr = []
-#     # i = min
-#     # while i < max
-#     #     arr << i
-#     #     i += 1
-#     # end
-#     # arr
+    # arr = []
+    # i = min
+    # while i < max
+    #     arr << i
+    #     i += 1
+    # end
+    # arr
 
 
-# end
+end
 
 
-# p Warmup(1,5)
+p Warmup(1,5)
 
 
 def Warmup(min, max)
@@ -26,3 +26,32 @@ end
 
 
 p Warmup(1,5)
+
+
+
+def exponentiation(base,exponent)
+    if exponent == 0
+        return 1
+    end
+
+    base * exponentiation(base, exponent - 1)
+end
+
+def exponentiation(base,exponent)
+    return 1 if exponent == 0
+
+    if exponent % 2 == 0 
+      exponentiation(base, exponent/2) **2
+      
+    else
+        base * (exponentiation(base,(exponent-1)/2)**2)
+    end
+
+  
+
+end
+
+p exponentiation(3,2)
+
+
+
